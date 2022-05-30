1662ae630e0021897190aefb5d5a84aa80fc4032
Tako <takogiorgobiani97@gmail.com>
2022-05-30 23:30:09 +0400

    fix 1

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 function showMessage() {
-    document.getElementById("result").style.display = "block";
+  document.getElementById("result").style.display = "block";
 }
 
 function compute() {
-
   var principal = document.getElementById("principal").value;
 
-  if(parseInt(principal) < 1) {
-      alert("Enter a valid positive number");
-      document.getElementById("principal").focus();
-      return();
+  if (parseInt(principal) < 1) {
+    alert("Enter a valid positive number");
+    document.getElementById("principal").focus();
+    return;
   }
 
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
+
   var interest = (principal * years * rate) / 100;
 
-  var today = newDate();
+  var today = new Date();
   var yearN = parseInt(today.getFullYear()) + parseInt(years);
 
-  document.getElementById("depositmessage").innerHTML = "" + principal;
-  document.getElementById("interestmessage").innerHTML = "" + rate;
-  document.getElementById("amountmessage").innerHTML = "" + interest;
-  document.getElementById("yearmessage").innerHTML = "" + yearN;
+  document.getElementById("deposit").innerHTML = "" + principal;
+  document.getElementById("interest").innerHTML = "" + rate;
+  document.getElementById("amount").innerHTML = "" + interest;
+  document.getElementById("year").innerHTML = "" + yearN;
   showMessage();
 
   return false;
-
-
-};
+}
 
 function updateRate() {
-  var rateval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerHTML = rateval;
+  var rate = document.getElementById("rate").value;
+  document.getElementById("ratevalue").innerHTML = rate;
 }
+
+console.log(rateval);
