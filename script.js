14e9f4dfd2c182f0475fe88d6dc5d36dff501066
Tako <takogiorgobiani97@gmail.com>
2022-05-31 00:12:27 +0400

    fix 4

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ function showMessage() {
 function compute() {
   var principal = document.getElementById("principal").value;
 
-  if (principal.value < 1) {
-    alert("Enter a valid positive number");
-    principal.focus();
+  if (parseInt(principal) < 1) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
     return;
   }
 
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
 
-  var interest = (principal.value * years.value * rate.value) / 100;
+  var interest = (principal * years * rate) / 100;
 
   var today = new Date();
-  var yearN = new Date().getFullYear() + parseInt(years.value);
+  var yearN = parseInt(today.getFullYear()) + parseInt(years);
 
-  document.getElementById("deposit").innerHTML = "" + principal;
-  document.getElementById("interest").innerHTML = "" + rate;
+  document.getElementById("depositValue").innerHTML = "" + principal;
+  document.getElementById("interestValue").innerHTML = "" + rate;
   document.getElementById("amount").innerHTML = "" + interest;
   document.getElementById("yearReceive").innerHTML = "" + yearN;
   showMessage();
@@ -32,5 +32,3 @@ function showRange() {
   var rate = document.getElementById("rate").value;
   document.getElementById("ratevalue").innerHTML = rate;
 }
-
-console.log(rate);
